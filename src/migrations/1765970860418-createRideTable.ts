ca28b3630346809c07f7723b15a4745a61796bfd
Laxman Rumba <rumbavai767@gmail.com>
2025-12-17 17:43:02 +0545

    chore: Add ride-request and trips migration

---
+++ src/migrations/1765970860418-createRideTable.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateRideTable1765970860418 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE "ride_requests" (
+            "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+            "passenger_id" uuid NOT NULL,
+            "destination" varchar (255) NOT NULL,
+            "landmark" varchar (255),
+            "pickup_location" varchar (255) NOT NULL,
+            "notes" varchar (3000),
+            "departure_time" TSTZRANGE NOT NULL,
+            "created_at" timestamp with time zone DEFAULT now(),
+            "deleted_at" timestamp with time zone DEFAULT NULL,
+            "updated_at" timestamp with time zone DEFAULT NULL,
+            CONSTRAINT "fk_users_ride_requests"
+            FOREIGN KEY ("passenger_id")
+            REFERENCES "users" ("user_id") ON DELETE CASCADE            
+        );
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`    
+      DROP TABLE "ride_requests";`);
+  }
+}
---
+++ src/migrations/1765971424543-createTripsTable.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTripsTable1765971424543 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE "trips" (
+            "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+            "request_id" uuid NOT NULL,
+            "driver_id" uuid NOT NULL,
+            "status" varchar (255) DEFAULT 'Not Started',
+            "vehicle_type" varchar (255) DEFAULT 'Two Wheeler',
+            "created_at" timestamp with time zone DEFAULT now(),
+            "deleted_at" timestamp with time zone DEFAULT NULL,
+            "updated_at" timestamp with time zone DEFAULT NULL,
+            CONSTRAINT "fk_users_trips"
+            FOREIGN KEY ("driver_id")
+            REFERENCES "users" ("user_id") ON DELETE CASCADE 
+            
+            CONSTRAINT "fk_request_rides_trips"
+            FOREIGN KEY ("request_id")
+            REFERENCES "request_rides" ("id") ON DELETE CASCADE   
+            
+            CREATE INDEX "IDX_trips_status" ON "trips" ("status");
+            CREATE INDEX "IDX_trips_vehicle_type" ON "trips" ("vehicle_type");
+
+        );
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(` \
+            DROP INDEX IF EXISTS "IDX_trips_status";
+            DROP INDEX IF EXISTS "IDX_trips_vehicle_type";
+      DROP TABLE "trips";`);
+  }
+}
