9be477cd48dee23e99c59335f263fb5d61524154
thisme8 <stutiupreti8@gmail.com>
2025-12-24 16:19:11 +0545

    fix(trips) : Add validation to the response data

--- src/trip/dto/get-trips-by-driver-data.ts
+++ src/trip/dto/get-trips-by-driver-data.ts
@@ -1,11 +1,34 @@
+/* eslint-disable prettier/prettier */
+import { Type } from 'class-transformer';
 import { Trip } from '../entities/trip.entity';
+import { IsObject, IsPhoneNumber, IsString, IsUrl, MaxLength, ValidateNested } from 'class-validator';
 
-export class GetTripsByDriverResponseDto extends Trip {
-  driver: {
-    firstName: string | null;
-    lastName: string | null;
-    phoneNumber: string | null;
-    profileImage: string | null;
-    primaryLocation: string | null;
-  };
+export class Driver {
+  @IsString()
+  @MaxLength(255)
+  firstName?: string | null;
+
+  @IsString()
+  @MaxLength(255)
+  lastName?: string | null;
+
+  @IsPhoneNumber()
+  @MaxLength(255)
+  phoneNumber?: string | null;
+
+  @IsUrl()
+  @MaxLength(255)
+  profileImage?: string | null;
+
+  @IsString()
+  @MaxLength(255)
+  primaryLocation?: string | null;
+}
+
+export class GetTripsByDriverResponseDto extends Trip{
+    @IsObject()
+    @ValidateNested()
+    @Type(()=>Driver)
+    driver : Driver
+    
 }
